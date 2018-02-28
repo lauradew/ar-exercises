9a74c96be9a897f7a64f8e56b2e63406888fa244
Laura De Witt <lauramdewitt@gmail.com>
2018-02-28 00:22:56 +0000

    store with employees can't be destroyed

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,14 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates_length_of :name, minimum: 3
   validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+
+  before_destroy :check_for_employees
+
+  private
+    def check_for_employees
+      if self.employees.count > 0
+        throw :abort
+      end
+    end
+
 end
